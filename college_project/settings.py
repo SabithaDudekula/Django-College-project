ab217c221d1f5ebfd1bcf621ced616e709112da6
SabithaDudekulagit config --global user.email dudekulasabitha@gmail.com <dudekulasabitha@gmail.com>
2025-11-20 15:53:52 +0530

    Update requirements for Render

--- college_project/settings.py
+++ college_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&pz@b=pb2usk%v*4h(7wcd)1pmjqooemnjy3o7#t8s&@8)sx&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ADD THIS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -124,3 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_URL = '/login/'
+
+
+import os
+if os.environ.get('RENDER'):
+    ALLOWED_HOSTS = ['*']
