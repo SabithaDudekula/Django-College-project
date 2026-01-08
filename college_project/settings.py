544c03864937841235356da08e6e08d9d39f5551
SabithaDudekulagit config --global user.email dudekulasabitha@gmail.com <dudekulasabitha@gmail.com>
2026-01-08 11:48:34 +0530

    Temporary enable DEBUG to trace Render error

--- college_project/settings.py
+++ college_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&pz@b=pb2usk%v*4h(7wcd)1pmjqooemnjy3o7#t8s&@8)sx&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
