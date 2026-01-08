690964abc11eb6b4f40a8245bbd5927b5c1c74ef
SabithaDudekulagit config --global user.email dudekulasabitha@gmail.com <dudekulasabitha@gmail.com>
2026-01-08 12:15:14 +0530

    Force migrate on startup for Render free plan

--- college_project/wsgi.py
+++ college_project/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_project.settings')
 
 application = get_wsgi_application()
+
+# 🚨 FORCE MIGRATIONS ON STARTUP (RENDER FREE PLAN FIX)
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
